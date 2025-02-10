7432fd3ec61a9e031b237e9a25f09eeb7bf1d0a0
scmjazzzz <dev.cheolmin@gmail.com>
2025-02-10 17:15:58 +0900

    feat: text-field 컴포넌트 작업

--- client/src/styles/sprinkles/properties/responsive.css.ts
+++ client/src/styles/sprinkles/properties/responsive.css.ts
@@ -10,6 +10,8 @@ export const responsiveProperties = defineProperties({
   },
   defaultCondition: 'mobile',
   properties: {
+    display: ['flex'],
+    flexDirection: ['column'],
     width: {
       full: '100%',
     },
