951f7b7d67223dcf79eb16aee258a440b767c34f
scmjazzzz <dev.cheolmin@gmail.com>
2025-02-19 23:03:32 +0900

    feat: active-link 컴포넌트 작업

---
+++ client/src/shared/components/active-link/active-link.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import type { ReactNode } from 'react'
+import { usePathname } from 'next/navigation'
+import { Link, type Props as LinkProps } from '../link'
+
+type Props = Omit<LinkProps, 'className' | 'children'> & {
+  className?: string | ((props: { isActive: boolean }) => string)
+  children?: ReactNode | ((props: { isActive: boolean }) => ReactNode)
+}
+
+export function ActiveLink({ href, className, children, ...props }: Props) {
+  const pathname = usePathname()
+  const isActive = href === pathname
+  const style = typeof className === 'function' ? className({ isActive }) : className
+  const content = typeof children === 'function' ? children({ isActive }) : children
+
+  return (
+    <Link href={href} className={style} {...props}>
+      {content}
+    </Link>
+  )
+}
