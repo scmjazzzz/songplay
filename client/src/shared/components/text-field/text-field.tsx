b9985d201d458285a4c285b023c28ccaa51bc03a
scmjazzzz <dev.cheolmin@gmail.com>
2025-03-01 14:26:10 +0900

    feat: text-field 컴포넌트 작업

---
+++ client/src/shared/components/text-field/text-field.tsx
@@ -0,0 +1,30 @@
+import { useId } from 'react'
+import { Input, type Props as InputProps } from '../input'
+import { container, heading } from './text-field.css'
+import { Text } from '../text'
+
+type Props = InputProps & {
+  label?: string
+  errorMessage?: string
+}
+
+export function TextField({ label, id, errorMessage, ...props }: Props) {
+  const createdId = useId()
+  const fieldId = id || `text-field-${createdId}`
+
+  return (
+    <div className={container}>
+      {label && (
+        <label htmlFor={fieldId} className={heading}>
+          {label}
+        </label>
+      )}
+      <Input id={fieldId} {...props} />
+      {errorMessage && (
+        <Text variant="textRegular2" color="danger" sx={{ paddingTop: 8 }}>
+          {errorMessage}
+        </Text>
+      )}
+    </div>
+  )
+}
