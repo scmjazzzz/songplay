6b0381762423a81738c6945c156a42b183dc53eb
scmjazzzz <dev.cheolmin@gmail.com>
2025-03-01 16:47:43 +0900

    fix: 하위 height 100% 안먹는 이슈 수정

--- client/src/layouts/layout/mobile/mobile-layout.css.ts
+++ client/src/layouts/layout/mobile/mobile-layout.css.ts
@@ -1,12 +1,26 @@
 import { sprinkles } from '@/styles/sprinkles'
+import { vars } from '@/styles/vars.css'
+import { style } from '@vanilla-extract/css'
 
-export const container = sprinkles({
-  display: 'flex',
-  flexDirection: 'column',
-  minHeight: 'screen',
-})
+export const container = style([
+  sprinkles({
+    display: 'flex',
+    flexDirection: 'column',
+    height: 'full',
+  }),
+  {
+    paddingTop: vars.base.mobile_header_height,
+    paddingBottom: vars.base.bottom_navigation_bar_height,
+  },
+])
 
-export const main = sprinkles({
-  flex: 1,
-  backgroundColor: 'default_background',
-})
+export const main = style([
+  sprinkles({
+    flex: 1,
+    backgroundColor: 'default_background',
+  }),
+  {
+    paddingTop: vars.base.mobile_layout_gap,
+    paddingBottom: vars.base.mobile_layout_gap,
+  },
+])
