ea168c1d46cd1bd5fa7bf17f969d21eca479c222
scmjazzzz <dev.cheolmin@gmail.com>
2025-03-08 01:51:06 +0900

    feat: text-field 컴포넌트 작업

---
+++ client/src/components/text-field/text-field.css.ts
@@ -0,0 +1,14 @@
+import { sprinkles } from '@/styles/sprinkles'
+
+export const container = sprinkles({
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+export const heading = sprinkles({
+  paddingBottom: 6,
+  fontSize: 16,
+  fontWeight: 'bold',
+  lineHeight: 'medium',
+  color: 'foreground_default',
+})
