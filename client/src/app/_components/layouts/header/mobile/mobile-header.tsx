1ef93427a387f6300080941d8733fc99fe94888a
scmjazzzz <dev.cheolmin@gmail.com>
2025-03-07 19:38:59 +0900

    feat: blintTitle 이 있을 경우 title 엘리먼트 변경

--- client/src/app/_components/layouts/header/mobile/mobile-header.tsx
+++ client/src/app/_components/layouts/header/mobile/mobile-header.tsx
@@ -12,7 +12,7 @@ export type Props = {
 export function MobileHeader({ title = 'songplay', blindTitle, left, right }: Props) {
   return (
     <header className={header}>
-      <Text as="h1" variant="textBold6" color="default">
+      <Text as={blindTitle ? 'strong' : 'h1'} variant="textBold6" color="default">
         {title}
       </Text>
       {blindTitle && <h1 className={blind}>{blindTitle}</h1>}
