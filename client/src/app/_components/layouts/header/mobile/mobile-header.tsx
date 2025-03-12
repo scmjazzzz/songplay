0bf538393b153a6d0e28c1748f2ec28b250c3744
scmjazzzz <dev.cheolmin@gmail.com>
2025-03-12 15:54:31 +0900

    feat: blindTitle props 추가

--- client/src/app/_components/layouts/header/mobile/mobile-header.tsx
+++ client/src/app/_components/layouts/header/mobile/mobile-header.tsx
@@ -1,18 +1,21 @@
 import { Text } from '@/shared/components/text'
+import { blind } from '@/styles/styles.css'
 import { header, side } from './mobile-header.css'
 
 export type Props = {
   title?: string
+  blindTitle?: string
   left?: React.ReactNode
   right?: React.ReactNode
 }
 
-export function MobileHeader({ title = 'songplay', left, right }: Props) {
+export function MobileHeader({ title = 'songplay', blindTitle, left, right }: Props) {
   return (
     <header className={header}>
-      <Text as="h1" variant="textBold6" color="default">
+      <Text as={blindTitle ? 'strong' : 'h1'} variant="textBold6" color="default">
         {title}
       </Text>
+      {blindTitle && <h1 className={blind}>{blindTitle}</h1>}
       {left && <div className={side({ direction: 'left' })}>{left}</div>}
       {right && <div className={side({ direction: 'right' })}>{right}</div>}
     </header>
